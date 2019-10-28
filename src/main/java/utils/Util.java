cab9d849b766788e3235afcbbc75daec6ab07dbf
FalcoConstantine <integro@inbox.lt>
2019-10-28 22:09:34 +0200

    Util class was modified

--- src/main/java/utils/Util.java
+++ src/main/java/utils/Util.java
@@ -1,5 +1,10 @@
 package utils;
 
-public class Util {
+import base.BaseTest;
+
+public class Util extends BaseTest{
+	
+	public static long PAGE_LOAD_WAIT = 20;
+	public static long IMPLICIT_WAIT = 10;
 
 }
