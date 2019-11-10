25d44f87542dad78a6eb3239c575306d64766a69
FalcoConstantine <integro@inbox.lt>
2019-11-10 09:41:25 +0200

    MainPageTest.java class was updated

--- src/test/java/tests/MainPageTest.java
+++ src/test/java/tests/MainPageTest.java
@@ -31,16 +31,22 @@ public class MainPageTest extends Base{
 	 *   -- Documentation --
 	 *   Test for Main Page main menu smoke test
 	 */
-	@Test(priority = 1, enabled = true, groups = {"smoke","mainPage_menu"})
+	@Test(priority = 1, enabled = true, groups = {"smoke","mainPage_menu_smoke"})
 	public void mainPageMenuTest_SMOKE() {
 		mainPage = new MainPage();
 		mainPage.mainMenu_SMOKE();
 	}
 	
-	@Test(priority = 2, enabled = true, groups = {"smoke","mainPage_search"})
+	@Test(priority = 2, enabled = true, groups = {"smoke","mainPage_search_smoke"})
 	public void mainPageStaysSearchTest_SMOKE() {
 		mainPage = new MainPage();
 		mainPage.search_SMOKE();
+	}
+	
+	@Test(priority = 3, enabled = true, groups = {"functional","mainPage_menu_functional"})
+	public void mainPageMenuTest_FUNCTIONAL() {
+		mainPage = new MainPage();
+		mainPage.mainMenu_FUNCTIONAL();
 	}
 	
 	
