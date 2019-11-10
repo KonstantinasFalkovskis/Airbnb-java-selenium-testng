b90dce236eab8e9ef7b4fe7638c5704c4d7d934b
FalcoConstantine <integro@inbox.lt>
2019-11-10 09:29:49 +0200

    Click utility modified

--- src/main/java/utils/ClickUtils.java
+++ src/main/java/utils/ClickUtils.java
@@ -1,14 +1,38 @@
 package utils;
 
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebElement;
 
 import base.Base;
 
 public class ClickUtils extends Base{
 	
-	public void doClick(WebElement element, int clickCount) {
+	JavascriptExecutor js = (JavascriptExecutor) driver;
+	
+	public void doMultiClick(WebElement element, int clickCount) {
 		for(int i = 0; i <= clickCount; i++ ) {
+			try {
+				doClick(element);
+			} catch(WebDriverException e) {
+				doClickWithJS(element);
+			}
+		}
+	}
+	
+	public void doClick(WebElement element) {
+		try {
 			element.click();
+		} catch(Exception e) {
+			e.printStackTrace();	
+		}
+	}
+	
+	public void doClickWithJS(WebElement element) {
+		try {
+			js.executeScript("arguments[0].click()", element);
+		} catch(Exception e) {
+			e.printStackTrace();
 		}
 	}
 
