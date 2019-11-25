c89a7997c86f500174ea36a06a8ac762398c7a78
FalcoConstantine <integro@inbox.lt>
2019-11-25 22:14:47 +0200

    jenkins pipeline is done

--- jenkins-pipeline.groovy
+++ jenkins-pipeline.groovy
@@ -9,12 +9,7 @@ node {
    
    stage('Build') {
       // Run the maven build
-      withEnv(["MVN_HOME=$mvnHome"]) {
-         if (isUnix()) {
-            sh '"$MVN_HOME/bin/mvn" -Dmaven.test.failure.ignore clean package'
-         } else {
-            bat(/"%MVN_HOME%\bin\mvn" -Dmaven.test.failure.ignore clean package/)
-         }
+      sh "mvn clean build"
       }
    
     stage('Smoke') {
