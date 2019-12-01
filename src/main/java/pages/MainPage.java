cbc767cb49def6ced1eaa01e41e22464389c67e7
FalcoConstantine <integro@inbox.lt>
2019-12-01 13:06:11 +0200

    +

--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -27,7 +27,7 @@ public class MainPage extends Base{
 	Actions builder = new Actions(driver);
 	
 	//-------------------------------------------------------->	
-	@FindBy(xpath = "//input[@id='Koan-magic-carpet-koan-search-bar__input_TEST']")
+	@FindBy(xpath = "//input[@id='Koan-magic-carpet-koan-search-bar__input']")
 	WebElement placeToGo;
 	
 	@FindBy(id = "checkin_input")
