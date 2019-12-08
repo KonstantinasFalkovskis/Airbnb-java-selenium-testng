c5d71be2587908915a4a3983d4b6822d204e4715
FalcoConstantine <integro@inbox.lt>
2019-12-08 14:37:39 +0200

    +

--- src/main/java/base/Base.java
+++ src/main/java/base/Base.java
@@ -13,6 +13,7 @@ import org.openqa.selenium.WebDriver;
 //import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.ITestContext;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Optional;
@@ -91,6 +92,11 @@ public class Base {
 			System.out.println("Some exception occurred while quitting the browser" + e);
 			log.info("[Some exception occurred while quitting the browser]" + e);
 		}
+	}
+	
+	@AfterClass()
+	public void closingAll() {
+		driver.close();
 	}
 
 }
