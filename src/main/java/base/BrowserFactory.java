f9cfa6404cf06f085f1e599517bc53c836265386
Falco Constantine <integro@inbox.lt>
2020-02-21 18:11:41 +0200

    Update BrowserFactory.java

--- src/main/java/base/BrowserFactory.java
+++ src/main/java/base/BrowserFactory.java
@@ -63,9 +63,11 @@ public class BrowserFactory {
 		
 //		public WebDriver parallelRun(String platform, String browserName, String version) throws MalformedURLException {
 		public WebDriver parallelRun(String platform, String browserName) throws MalformedURLException {
-			
-			String hubURLWin = "http://192.168.0.166:4444/wd/hub";
+							
+			String hubURLWin = "http://192.168.0.161:4444/wd/hub";
 			String hubURLMac = "http://192.168.0.129:4444/wd/hub";
+			String hubURLLinux = "http://192.168.0.106:4444/wd/hub";
+
 			DesiredCapabilities caps = new DesiredCapabilities();
 			System.out.println("Starting " + browserName + " on grid");
 			
@@ -130,7 +132,8 @@ public class BrowserFactory {
 		
 			try {
 				//tdriver.set(new RemoteWebDriver(new URL(hubURLWin), caps));
-				tdriver.set(new RemoteWebDriver(new URL(hubURLMac), caps));
+				//tdriver.set(new RemoteWebDriver(new URL(hubURLMac), caps));
+				tdriver.set(new RemoteWebDriver(new URL(hubURLLinux), caps));;
 			} catch (MalformedURLException e) {
 				e.printStackTrace();
 			}
